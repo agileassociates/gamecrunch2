d52a7fec49731e158bd72cf9f7c9025ab117684b
Johny Babylon <Home@Johnys-MacBook.local>
2016-05-08 22:16:07 -0700

    added pin verification

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,2 +1,18 @@
 class PhoneNumber < ActiveRecord::Base
+  def generate_pin
+    self.pin = rand(0000..9999).to_s.rjust(4, "0")
+    save
+  end
+  def twilio_client
+    client = Twilio::REST::Client.new 'ACc37abcdd6d04384260291eb1f7075584', '8ef5aec33725886af6b1f9981bf86c0e'
+  end
+  def send_pin
+    twilio_client.messages.create(
+        to: phone_number,
+        from: '14159172576',
+        body: "Your PIN is #{pin}" )
+  end
+  def verify(entered_pin)
+    update(verified: true) if self.pin == entered_pin
+  end
 end
